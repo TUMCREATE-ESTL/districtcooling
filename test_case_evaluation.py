83e888e40f9590118eee030fdab5c399020ad7fc
Mischa Grussmann <mischa.grussmann@tum.de>
2019-11-18 20:30:02 +0800

    test_case_evaluation:updates

--- test_case_evaluation.py
+++ test_case_evaluation.py
@@ -17,7 +17,7 @@ TC_flex_0MWh = pd.read_csv(
     'results/csv_files/TESTCASE_BuildT=flex_TES=0MWh__solved_problem.csv',
     index_col=[0]
 )
-"""
+
 TC_flex_625MWh = pd.read_csv(
     'results/csv_files/TESTCASE_BuildT=flex_TES=625MWh__solved_problem.csv',
     index_col=[0]
@@ -30,7 +30,7 @@ TC_flex_1875MWh = pd.read_csv(
     'results/csv_files/TESTCASE_BuildT=flex_TES=1875MWh__solved_problem.csv',
     index_col=[0]
 )
-"""
+
 TC_flex_2500MWh = pd.read_csv(
     'results/csv_files/TESTCASE_BuildT=flex_TES=2500MWh__solved_problem.csv',
     index_col=[0]
@@ -41,7 +41,7 @@ TC_flex_2500MWh = pd.read_csv(
 # Evaluation ===========================================================================================================
 
 # Costs comparison -----------------------------------------------------------------------------------------------------
-"""
+
 print(TC_flex_0MWh.loc['Costs in [S$]'].sum())
 costs_flex_dict = {
     '0': TC_flex_0MWh.loc['Costs in [S$]'].sum()*10**(-6),
@@ -62,7 +62,7 @@ plot_costs = costs_flex_df.plot(
     kind='bar',
     use_index=True,
     y='Overall year costs in [*10**6 S$]',
-    ylim=[24, 29],
+    ylim=[20, 30],
     legend=False,
     rot=0
 )
@@ -77,7 +77,45 @@ for i in plot_costs.patches:
 plot_costs.set_xlabel('TES capacities [MWh]')
 plot_costs.set_ylabel('Electricity costs of entire year [million S$]')
 plt.show()
-"""
+
+# Costs per GFA comparison ---------------------------------------------------------------------------------------------
+
+print(TC_flex_0MWh.loc['Costs in [S$]'].sum())
+costs_flex_dict = {
+    '0': TC_flex_0MWh.loc['Costs in [S$]'].sum()/1106260.4,
+    '625': TC_flex_625MWh.loc['Costs in [S$]'].sum()/1106260.4,
+    '1250': TC_flex_1250MWh.loc['Costs in [S$]'].sum()/1106260.4,
+    '1875': TC_flex_1875MWh.loc['Costs in [S$]'].sum()/1106260.4,
+    '2500': TC_flex_2500MWh.loc['Costs in [S$]'].sum()/1106260.4
+}
+costs_flex_df = pd.DataFrame.from_dict(
+    data=costs_flex_dict,
+    orient='index',
+    columns=['Overall year costs per GFA [S$/m2]']
+)
+print(
+    costs_flex_df
+)
+plot_costs = costs_flex_df.plot(
+    kind='bar',
+    use_index=True,
+    y='Overall year costs per GFA [S$/m2]',
+    ylim=[20, 27],
+    legend=False,
+    rot=0
+)
+# label bars
+for i in plot_costs.patches:
+    plot_costs.text(i.get_x()+0.05, i.get_height()+.05,
+        str(round(i.get_height(), 2)),
+        # fontsize=15,
+        # color='dimgrey'
+    )
+# label axis
+plot_costs.set_xlabel('TES capacities [MWh]')
+plot_costs.set_ylabel('Electricity costs of entire year per GFA [S$/m2 ]')
+plt.show()
+
 # Price to Power -------------------------------------------------------------------------------------------------------
 """
 print(TC_flex_2500MWh.loc['DCS total Power in [MW]'])
@@ -157,7 +195,7 @@ plt.ylabel('Frequency')
 plt.show()
 """
 # Histogram Costs ------------------------------------------------------------------------------------------------------
-
+"""
 histo_costs_2500MWh = TC_flex_2500MWh.loc['Costs in [S$]'].drop(['IDs'], axis=0).plot.hist(
     bins=100,
     alpha=1
@@ -175,6 +213,134 @@ histo_costs_0MWh = TC_flex_0MWh.loc['Costs in [S$]'].drop(['IDs'], axis=0).plot.
 plt.xlabel('Electricity costs of entire year [million S$]')
 plt.ylabel('Frequency')
 plt.show()
+"""
+# Scatter Power Time ---------------------------------------------------------------------------------------------------
+"""
+power_time_2500MWh = plt.scatter(
+    x=parameters.environment.index.values,
+    y=TC_flex_2500MWh.loc['DCS total Power in [MW]'].drop(['IDs'], axis=0).values,
+    marker='.',
+    s=1,
+    # c='darkgreen'
+)
+# label axis
+plt.xlabel('Time step [0.5 h]')
+plt.ylabel('DCS total Power (MW)')
+plt.show()
 
-# Scatter Costs --------------------------------------------------------------------------------------------------------
+power_time_0MWh = plt.scatter(
+    x=parameters.environment.index.values,
+    y=TC_flex_0MWh.loc['DCS total Power in [MW]'].drop(['IDs'], axis=0).values,
+    marker='.',
+    s=1,
+    # c='darkgreen'
+)
+# label axis
+plt.xlabel('Time step [0.5 h]')
+plt.ylabel('DCS total Power (MW)')
+plt.show()
+"""
+# Scatter Cost Time ---------------------------------------------------------------------------------------------------
+"""
+cost_time_2500MWh = plt.scatter(
+    x=parameters.environment.index.values,
+    y=TC_flex_2500MWh.loc['Costs in [S$]'].drop(['IDs'], axis=0).values,
+    marker='.',
+    s=1,
+    # c='darkgreen'
+)
+price_time = plt.scatter(
+    x=parameters.environment.index.values,
+    y=parameters.environment['Price [S$/MWh]'].values,
+    marker='.',
+    s=1,
+    c='r',
+    alpha=0.5
+)
+# label axis
+plt.xlabel('Time step [0.5 h]')
+plt.ylabel('Costs (S$)')
+plt.show()
 
+costs_time_0MWh = plt.scatter(
+    x=parameters.environment.index.values,
+    y=TC_flex_0MWh.loc['Costs in [S$]'].drop(['IDs'], axis=0).values,
+    marker='.',
+    s=1,
+    # c='darkgreen'
+)
+# label axis
+plt.xlabel('Time step [0.5 h]')
+plt.ylabel('Costs (S$)')
+plt.show()
+"""
+# Scatter ChS and TES Time ---------------------------------------------------------------------------------------------
+"""
+chsflow_time_2500MWh = plt.scatter(
+    x=parameters.environment.index.values,
+    y=TC_flex_2500MWh.loc['Chiller-set flow [qbm/s]'].drop(['IDs'], axis=0).values,
+    marker='.',
+    s=1,
+    # c='darkgreen'
+)
+
+etsflow_time_2500MWh = plt.scatter(
+    x=parameters.environment.index.values,
+    y=TC_flex_2500MWh.loc['TES flow [qbm/s]'].drop(['IDs'], axis=0).values,
+    marker='.',
+    s=1,
+    c='r'
+)
+
+# label axis
+plt.xlabel('Time step [0.5 h]')
+plt.ylabel('Volumetric flow (m3/s)')
+plt.show()
+
+chsflow_time_0MWh = plt.scatter(
+    x=parameters.environment.index.values,
+    y=TC_flex_0MWh.loc['Chiller-set flow [qbm/s]'].drop(['IDs'], axis=0).values,
+    marker='.',
+    s=1,
+    # c='darkgreen'
+)
+
+etsflow_time_0MWh = plt.scatter(
+    x=parameters.environment.index.values,
+    y=TC_flex_0MWh.loc['TES flow [qbm/s]'].drop(['IDs'], axis=0).values,
+    marker='.',
+    s=1,
+    c='r'
+)
+
+# label axis
+plt.xlabel('Time step [0.5 h]')
+plt.ylabel('Volumetric flow (m3/s)')
+plt.show()
+"""
+# Scatter ChS and TES --------------------------------------------------------------------------------------------------
+"""
+ets_chs_2500MWh = plt.scatter(
+    x=TC_flex_2500MWh.loc['Chiller-set flow [qbm/s]'].drop(['IDs'], axis=0).values,
+    y=TC_flex_2500MWh.loc['TES flow [qbm/s]'].drop(['IDs'], axis=0).values,
+    marker='.',
+    s=1,
+    # c='r'
+)
+# label axis
+plt.xlabel('Chiller-set flow [qbm/s]')
+plt.ylabel('TES flow [qbm/s]')
+plt.show()
+
+ets_chs_0MWh = plt.scatter(
+    x=TC_flex_0MWh.loc['Chiller-set flow [qbm/s]'].drop(['IDs'], axis=0).values,
+    y=TC_flex_0MWh.loc['TES flow [qbm/s]'].drop(['IDs'], axis=0).values,
+    marker='.',
+    s=1,
+    # c='r'
+)
+# label axis
+plt.xlabel('Time step [0.5 h]')
+plt.ylabel('Volumetric flow (m3/s)')
+plt.show()
+"""
